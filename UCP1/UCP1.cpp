3d1bdad97322880d803d5861c00cda0f71aba503
haikal0423 <mhaikal23azka@gmail.com>
2024-03-25 14:32:13 +0700

    membuat prosedur display

--- UCP1/UCP1.cpp
+++ UCP1/UCP1.cpp
@@ -42,6 +42,19 @@ void SelectionSortArray() { //procedur untuk mengurutkan array dengan metode sel
             swap(haikal[j], haikal[min_index]); //step 3
         }
 
+void display() {
+     cout << endl;
+     cout << "===================================" << endl;
+     cout << "Element Array yang telah tersusun" << endl;
+     cout << "===================================" << endl;
+     for (int j = 0; j < n; j++) {
+      cout << arr[j] << endl; // Output setiap element array pada garis baru
+
+      }
+       cout << "jumlah pass = " << n - 1 << endl; //menampilkan total angka dari elemen yang benar
+       cout << endl;
+ }
+
 int main()
 {
     
