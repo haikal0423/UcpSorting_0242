5d12a2bb9948f3c68be9b158d2dabe9a36a7b2a1
haikal0423 <mhaikal23azka@gmail.com>
2024-03-25 14:24:33 +0700

    Add project files.

---
+++ UCP1/UCP1.cpp
@@ -0,0 +1,38 @@
+
+
+#include <iostream>
+using namespace std;
+
+int haikal[42];
+int n;
+
+void input() {
+    while (true) {
+        cout << "masukan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 42)
+            break;
+        else {
+            cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+
+        }
+    }
+    cout << endl;
+    cout << "===============" << endl;
+    cout << "masukan Elemen Array" << endl;
+    cout << "===============" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "data ke-" << (i + 1) << ": ";
+        cin >> arr[i];
+
+    }
+
+}
+int main()
+{
+    
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+
